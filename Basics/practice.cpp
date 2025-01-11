92ea85e704aba7be9a383a32dfd76b36505e9875
Priyesh Mishra <mpriyesh235@gmail.com>
2025-01-11 18:05:59 +0530

    testing extensions

--- Basics/practice.cpp
+++ Basics/practice.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 
-int main(){
-
-    int favorite_number=0;
-    std::cout<<"Enter a number between 1 and 100: ";
-    std::cin>>favorite_number;
-    std::cout<<"Amazing! That is my favorite number too"<<std::endl;
-    std::cout<<"No, really ";
-    std::cout<< favorite_number;
-    std::cout<<" is my favorite number too"<<std::endl;
-    return 0;
+int main() {
+  int favorite_number = 0;
+  std::cout << "Enter a number between 1 and 100: ";
+  std::cin >> favorite_number;
+  std::cout << "Amazing! That is my favorite number too\nNo, really "
+            << favorite_number << " is my favorite number too." << std::endl;
+  return 0;
 }
