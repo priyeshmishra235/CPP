830e5e6183c50b5ec6f7767e90865c5f9ec3cd2d
Priyesh Mishra <mpriyesh235@gmail.com>
2025-01-22 03:16:36 +0530

    basic encryption decryption using string

---
+++ Basics/EDdecryption.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int main() {
+  // alphabet and key
+  std::string charSet{
+      " !#$%&'()*+,-./"
+      "0123456789:\";<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`"
+      "abcdefghijklmnopqrstuvwxyz{|}~"};
+  std::string key{
+      "W&h93q@Pa#->x8U]XI}s_fOH0wTm?<$=ZA7BEn,{/"
+      ";*~o%Yj`MD[plc 'dQgKu|\"RtiC)ze(:1k.V6NGL5ry+2bv!FS4^J"};
+
+  // getting message input
+  std::cout << "Enter the encrypted message you want to decrypt!: \n";
+  std::string message{};
+  std::getline(std::cin, message);
+
+  // decrypting the message
+  for (size_t i{0}; i < message.length(); i++) {
+    size_t index{key.find(message.at(i))};
+    message.at(i) = charSet.at(index);
+  }
+  std::cout << "\nDecrypted message: " << message;
+
+  return 0;
+}
---
+++ Basics/EDencryption.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int main() {
+  // alphabet and key
+  std::string charSet{
+      " !#$%&'()*+,-./"
+      "0123456789:\";<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`"
+      "abcdefghijklmnopqrstuvwxyz{|}~"};
+  std::string key{
+      "W&h93q@Pa#->x8U]XI}s_fOH0wTm?<$=ZA7BEn,{/"
+      ";*~o%Yj`MD[plc 'dQgKu|\"RtiC)ze(:1k.V6NGL5ry+2bv!FS4^J"};
+
+  // getting message input
+  std::cout << "Enter the message you want to encrypt!: \n";
+  std::string message{};
+  std::getline(std::cin, message);
+
+  // encrypting the message
+  for (size_t i{0}; i < message.length(); i++) {
+    size_t index{charSet.find(message.at(i))};
+    message.at(i) = key.at(index);
+  }
+  std::cout << "\nEncrypted message: " << message;
+
+  return 0;
+}
